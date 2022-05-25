390e6d38028a498067ba6cb13db004ceba22a30b
Mike Westley <mike.westley@samaritanministries.org>
2022-05-25 16:51:53 -0500

    Start here

---
+++ RomanNumeralsKata/Kata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RomanNumeralsKata
+{
+    public class Kata
+    {
+        public object getRomanNumeral(int number)
+        {
+            if (number == 1)
+            {
+                return "I";
+            }
+            else
+            {
+                if (number == 2)
+                {
+                    return "II";
+                }
+            }  
+            return "III";
+        }
+    }
+}
---
+++ RomanNumeralsKataTests/KataTests.cs
@@ -0,0 +1,39 @@
+﻿using RomanNumeralsKata;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace RomanNumeralsKataTests
+{
+    public class KataTests
+    {
+        private readonly Kata _kata;
+
+        public KataTests()
+        {
+            _kata = new Kata();
+        }
+
+        [Fact]
+        public void passingOneReturns_I()
+        {
+            var numeral = _kata.getRomanNumeral(1);
+
+            Assert.Equal("I", numeral);
+        }
+
+        [Fact]
+        public void passingTwoReturns_II()
+        {
+            var numeral = _kata.getRomanNumeral(2);
+
+            Assert.Equal("II", numeral);
+        }
+        [Fact]
+        public void passingThreeReturns_III()
+        {
+            var numeral = _kata.getRomanNumeral(3);
+
+            Assert.Equal("III", numeral);
+        }
+    }
+}
