e4f34f049ab90a87656da3a428b2d3d3de37b6df
Mike Westley <mike.westley@samaritanministries.org>
2022-06-09 14:04:58 -0500

    Replaced ugly if statment with for loop

--- RomanNumeralsKata/Kata.cs
+++ RomanNumeralsKata/Kata.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RomanNumeralsKata
 {
@@ -6,63 +7,29 @@ namespace RomanNumeralsKata
     {
         public string GetRomanNumeral(int number)
         {
-            var numeral="";
+            SortedList<int, string> romanNumerals = new SortedList<int, string>();
+            romanNumerals.Add(0,"Nulla");
+            romanNumerals.Add(1,    "I");
+            romanNumerals.Add(5,    "V");
+            romanNumerals.Add(10,   "X");
+            romanNumerals.Add(50,   "L");
+            romanNumerals.Add(100,  "C");
+            romanNumerals.Add(500,  "D");
+            romanNumerals.Add(1000, "M");
+
+            var numeral ="";
             var replacementChar = "";
             var divisor = 0;
 
-            if (number == 0)
+            for (int i = romanNumerals.Count - 1; i > -1; i--) // iterate descending from end of list
             {
-                return "Nulla";
-            }
+                Console.WriteLine("key: {0}, value: {1}", romanNumerals.Keys[i], romanNumerals.Values[i]);
 
-            if (number >= 1000)
-            { 
-                replacementChar = "M";
-                divisor = 1000;
-            }
-            else
-            {
-                if (number >= 500)
+                if (number >= romanNumerals.Keys[i])
                 {
-                    replacementChar = "D";
-                    divisor = 500;
-                }
-                else
-                {
-                    if (number >= 100)
-                    {
-                        replacementChar = "C";
-                        divisor = 100;
-                    }
-                    else
-                    {
-                        if (number >= 50)
-                        {
-                            replacementChar = "L";
-                            divisor = 50;
-                        }
-                        else
-                        {
-                            if (number >= 10)
-                            {
-                                replacementChar = "X";
-                                divisor = 10;
-                            }
-                            else
-                            {
-                                if (number >= 5)
-                                {
-                                    replacementChar = "V";
-                                    divisor = 5;
-                                }
-                                else
-                                {
-                                    replacementChar = "I";
-                                    divisor = 1;
-                                }
-                            }
-                        }
-                    }
+                    replacementChar = romanNumerals.Values[i];
+                    divisor = romanNumerals.Keys[i];
+                    i = -1;
                 }
             }
 
@@ -73,7 +40,7 @@ namespace RomanNumeralsKata
 
         private static int DetermineCharMultiplier(int number, int divisor)
         {
-            int charMultiplier = Math.DivRem(number, divisor, out int remainder);
+            int charMultiplier = divisor > 0 ? Math.DivRem(number, divisor, out int remainder): 1;
 
             return charMultiplier;
         }
